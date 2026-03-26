695976ad10a596c5bd52ded58e304f82549220fb
utilisateur <seydina.badiane@grenoble-inp.org>
2026-03-26 16:21:13 +0100

    level1

--- submissions/longcall/level1.py
+++ submissions/longcall/level1.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import Iterable
 
-from src.common.models import MultiBook, Order
+from src.common.models import MultiBook, Order, Side, BookSide
+from src.common.models import *
 
 
 def process_orders(initial_book: MultiBook, orders: Iterable[Order]) -> MultiBook:
@@ -28,4 +29,75 @@ def process_orders(initial_book: MultiBook, orders: Iterable[Order]) -> MultiBoo
     Returns :
         État final du MultiBook après traitement de tous les ordres.
     """
-    raise NotImplementedError("Implémenter le Palier 1 : Ordres Limite de Base")
+
+    def insert_in_book(side: BookSide, order: Order) -> None:
+        
+        inserted = False
+        
+        for i, existing in enumerate(side.orders):
+            if side.side == Side.BUY:
+                
+                if existing.price < order.price:
+                    side.orders.insert(i, order)
+                    inserted = True
+                    break
+                
+            else:
+                
+                if existing.price > order.price:
+                    side.orders.insert(i, order)
+                    inserted = True
+                    break
+        if not inserted:
+            side.orders.append(order)
+
+    for order in orders:
+        
+        my_order_book = initial_book.get_or_create(order.asset)
+
+        if order.side == Side.BUY:
+            asks = my_order_book.asks
+            
+            while order.quantity > 0:
+                best_ask = asks.best()
+                if best_ask is None:
+                    break
+                if best_ask.price <= order.price:
+                    if order.quantity < best_ask.quantity:
+                        best_ask.quantity -= order.quantity
+                        order.quantity = 0
+                    elif order.quantity == best_ask.quantity:
+                        order.quantity = 0
+                        asks.orders.pop(0)
+                    else:
+                       
+                        order.quantity -= best_ask.quantity
+                        asks.orders.pop(0)
+                else:
+                    break
+            
+            if order.quantity > 0:
+                insert_in_book(my_order_book.bids, order)
+
+        elif order.side == Side.SELL:
+            bids = my_order_book.bids
+            while order.quantity > 0:
+                best_bid = bids.best()
+                if best_bid is None:
+                    break
+                if best_bid.price >= order.price:
+                    if order.quantity < best_bid.quantity:
+                        best_bid.quantity -= order.quantity
+                        order.quantity = 0
+                    elif order.quantity == best_bid.quantity:
+                        order.quantity = 0
+                        bids.orders.pop(0)
+                    else:
+                        order.quantity -= best_bid.quantity
+                        bids.orders.pop(0)
+                else:
+                    break
+            if order.quantity > 0:
+                insert_in_book(my_order_book.asks, order)
+    return initial_book
+    # raise NotImplementedError("Implémenter le Palier 1 : Ordres Limite de Base")
