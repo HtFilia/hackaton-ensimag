c8ef835759dcaf37a4fcca8ed4ee55dce7ee4735
faddeh <hassane.fadde@grenoble-inp.org>
2026-03-26 16:55:57 +0100

    level 3e

--- submissions/flos/level3.py
+++ submissions/flos/level3.py
@@ -32,4 +32,112 @@ def process_orders(initial_book: MultiBook, orders: Iterable[Order]) -> MultiBoo
     Returns :
         État final du MultiBook.
     """
-    raise NotImplementedError("Implémenter le Palier 3 : Annulation et Modification")
+    for order in orders:
+        orderBook = initial_book.get_or_create(order.asset)
+        if order.action == "NEW":
+            if order.order_type == "market":
+                if order.side == "sell":
+                    bid_index = 0
+                    while bid_index < len(orderBook.bids.orders):
+                        bid = orderBook.bids.orders[bid_index]
+                        if bid.quantity > order.quantity:
+                            bid.quantity-= order.quantity
+                            order.quantity = 0
+                            break
+                        else:
+                            order.quantity-=bid.quantity
+                            orderBook.bids.orders.pop(bid_index)
+                            continue
+                else:
+                    ask_index = 0
+                    while ask_index < len(orderBook.asks.orders):
+                        ask = orderBook.asks.orders[ask_index]
+                        if ask.quantity > order.quantity:
+                            ask.quantity-= order.quantity
+                            order.quantity = 0
+                            break
+                        else:
+                            order.quantity-=ask.quantity
+                            orderBook.asks.orders.pop(ask_index)
+                            continue
+            else :
+                if order.side == "sell":
+                    bid_index = 0
+                    while bid_index < len(orderBook.bids.orders):
+                        bid = orderBook.bids.orders[bid_index]
+                        if bid.price >= order.price:
+                            if bid.quantity > order.quantity:
+                                bid.quantity-= order.quantity
+                                order.quantity = 0
+                                break
+                            else:
+                                order.quantity-=bid.quantity
+                                orderBook.bids.orders.pop(bid_index)
+                                continue
+                        break
+                    if order.quantity > 0:
+                        orderBook.asks.add(order)
+                else :
+                    ask_index = 0
+                    while ask_index < len(orderBook.asks.orders):
+                        ask = orderBook.asks.orders[ask_index]
+                        if ask.price <= order.price:
+                            if ask.quantity > order.quantity:
+                                ask.quantity-= order.quantity
+                                order.quantity = 0
+                                break
+                            else:
+                                order.quantity-=ask.quantity
+                                orderBook.asks.orders.pop(ask_index)
+                                continue
+                        break
+                    if order.quantity > 0:
+                        orderBook.bids.add(order)
+        elif order.action =="CANCEL":
+            if order.side == "buy":
+                bid_index = 0
+                while bid_index < len(orderBook.bids.orders):
+                    if orderBook.bids.orders[bid_index].id == order.id:
+                        orderBook.bids.orders.pop(bid_index)
+                        break
+                    bid_index+=1
+            else:
+                ask_index = 0
+                while ask_index < len(orderBook.asks.orders):
+                    if orderBook.asks.orders[ask_index].id == order.id:
+                        orderBook.asks.orders.pop(ask_index)
+                        break
+                    bid_index+=1
+        else:
+            if order.side == "buy":
+                bid_index = 0
+                while bid_index < len(orderBook.bids.orders):
+                    bid = orderBook.bids.orders[bid_index]
+                    if bid.id == order.id:
+                        if bid.price == order.price:
+                            bid.quantity = order.quantity
+                        else:
+                            bid.price = order.price
+                            bid.quantity = order.quantity
+                            orderBook.bids.orders.pop(bid_index)
+                            orderBook.bids.add(bid)
+                        break
+                    bid_index+=1
+            else :
+                ask_index = 0
+                while ask_index < len(orderBook.asks.orders):
+                    ask = orderBook.asks.orders[ask_index]
+                    if ask.id == order.id:
+                        if ask.price == ask.price:
+                            ask.quantity = order.quantity
+                        else:
+                            ask.price = order.price
+                            ask.quantity = order.quantity
+                            orderBook.asks.orders.pop(ask_index)
+                            orderBook.asks.add(ask)
+                        break
+                    ask_index+=1
+    return initial_book
+
+
+
